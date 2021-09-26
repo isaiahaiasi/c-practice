f6424fd5bb94f282fc3a6eba435655a1a00dcb82
Isaiah <computerblender@gmail.com>
2021-09-25 18:07:49 -0700

    Refactor detab program

--- src/kr-1/detab.c
+++ src/kr-1/detab.c
@@ -2,18 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEFAULT_WIDTH 4
+
+int get_spaces(int argc, char* args[argc]);
+void print_output(int spaces);
+
 // replace tabs with a set number of spaces (default: 4)
 // ex: detab 2 < input.c > output.c
 int main(int argc, char* args[argc]) {
-  // set the number of spaces that counts as a tab (default 4)
-  int spaces = 4;
-  if (argc > 1) {
-    spaces = atoi(args[1]);
-  }
+  int spaces = get_spaces(argc, args);
+  print_output(spaces);
+}
 
+// set the number of spaces that counts as a tab (default 4)
+int get_spaces(int argc, char* args[argc]) {
+  return argc > 1 ? atoi(args[1]) : DEFAULT_WIDTH;
+}
+
+// output the input char-by-char, replacing '\t's with N spaces
+void print_output(int spaces) {
   int c;
   while ((c = getchar()) != EOF) {
-    // if c is a tab, instead of printing c print spaces
     if (c == '\t') {
       for (int i = 0; i < spaces; ++i) {
         putchar(' ');
